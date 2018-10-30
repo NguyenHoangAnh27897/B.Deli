a737e84c639462745a88db66b5a0b299877bea02
PhuongTrang013 <phuongtrang1397@gmail.com>
2018-10-30 17:14:21 +0700

    updateBdeli

--- bdeli/Controllers/WebMaster/VideoController.cs
+++ bdeli/Controllers/WebMaster/VideoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using bdeli.Models;
 using System.Web.Mvc;
+using System.IO;
 
 namespace bdeli.Controllers.WebMaster
 {
@@ -24,13 +25,30 @@ namespace bdeli.Controllers.WebMaster
         }
 
         [HttpPost]
-        public ActionResult Edit(string title, string des, HttpPostedFileBase[] images)
+        public ActionResult Edit(string title, string des, HttpPostedFileBase video)
         {
             if (Session["Authentication"] != null)
             {
+                string Video = "";
+                if (video != null)
+                {
+                    if (video.ContentLength > 0)
+                    {
+                        var filename = Path.GetFileName(video.FileName);
+                        var fname = filename.Replace(" ", "_");
+                        var path = Path.Combine(Server.MapPath("~/Video"), fname);
+                        video.SaveAs(path);
+                        Video += fname;
+                    }
+
+                }
                 var home = db.bD_Video.Find(1);
                 home.Description = des;
                 home.Title = title;
+                if (Video != null)
+                {
+                    home.Video = Video;
+                }
                 db.Entry(home).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Edit");
